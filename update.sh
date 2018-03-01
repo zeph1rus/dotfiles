5c724c1854fdfc8f8c0d34c7122a69871b5a2027
Jon Truran <jon.truran@gmail.com>
2018-03-01 14:44:03 +0000

    Pushed into script

--- update.sh
+++ update.sh
@@ -1,44 +1,62 @@
 #!/bin/bash
 # Script to update local config files and do cleanup etc
-
-if [ -f ~/.vimrc ]; then
-	echo "Updating vimrc"
-	if [ -f ~/.vimrc.bak ]; then
-		echo "Removing Old vimrc backup"
-		rm ~/.vimrc.bak
-	fi
-	if [ -L ~/.vimrc ]; then
-		echo "VimRC is a symlink, fix manually if an error"
+manage_dotfile () {
+	#substitution doesn't work properly unless named variable
+	filetomanage=$1
+	#must use $HOME not ~ as otherwise scope causes breakage
+	abspathtomanage="$HOME/$filetomanage"
+	abspathtobackup="$HOME/$filetomanage.df"
+	dotfilessource="$HOME/dotfiles/$2"
+	echo "Managing file $filetomanage from $dotfilessource"
+	if [ -f $abspathtomanage ]; then
+		echo "$filetomanage exists"
+		#check if there is a symlink available
+		if [ -L $abspathtomanage ]; then
+			echo "is a symlink"
+			#check if is in the file
+			if grep -qF "$filetomanage" $HOME/.dotfiles; then
+				echo "Managed By dotfiles, leaving"
+			else
+				echo "Removing existing symlink"
+				rm $abspathtomanage
+				echo "Managing file ${filetomanage:1}"
+				echo "$filetomanage\n" >> $HOME/.dotfiles
+				ln -s $dotfilessource $abspathtomanage
+			fi
+		else
+			echo "Existing file not symlink, backing up"
+		 	
+			if [ -f $abspathtobackup ]; then
+				echo "Existing Backup Detected, removing"
+				rm $abspathtobackup
+			fi
+			echo "Backed up to $abspathtobackup"
+			mv $abspathtomanage $abspathtobackup
+			echo "Managing file"
+			echo "$filetomanage" >> $HOME/.dotfiles
+			ln -s $dotfilessource $abspathtomanage
+		fi
 	else
-		mv ~/.vimrc ~/.vimrc.bak
-		ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+		echo "File does not exist"
+		echo "$filetomanage\n" >> $HOME/.dotfiles
+		echo "Creating New Symlink and managing"	
+		ln -s $dotfilessource $abspathtomanage
 	fi
-else
-	echo "Creating new vimrc"
-	ln -s ~/dotfiles/vim/vimrc ~/.vimrc
-fi
 
-echo "Updating VIM Plugins"
+}
 
+#check if Vundle is Installed
 if [ -d ~/.vim/bundle/Vundle.vim ]; then
 	git -C ~/.vim/bundle/Vundle.vim pull
 else
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	got clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
 fi
 
+
+manage_dotfile ".vimrc" "vim/vimrc"
+manage_dotfile ".bashrc" "bashrc"
+manage_dotfile ".tmux.conf" "tmux.conf"
+
+#updating 
 vim +PluginInstall +qall
 
-echo "updating bashrc"
-if [ -f ~/.bashrc ]; then
-	if [ -L ~/.bashrc ]; then
-		echo "BashRC is a symlink, left alone - update manually if not correct"
-	else
-		echo "moving bashrc (will be sourced)"
-		mv ~/.bashrc ~/.bashrc.df
-		echo "symlink replacing bashrc"
-		ln -s ~/dotfiles/bashrc ~/.bashrc
-	fi
-fi
-
-echo "Reloading bashrc"
-exec bash
