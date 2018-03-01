99c802cc442fce2d566935bc64ac4e2039565799
Jon Truran <jon.truran@gmail.com>
2018-03-01 15:53:53 +0000

    fixed obvious typo

--- update.sh
+++ update.sh
@@ -49,7 +49,7 @@ manage_dotfile () {
 if [ -d ~/.vim/bundle/Vundle.vim ]; then
 	git -C ~/.vim/bundle/Vundle.vim pull
 else
-	got clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
 fi
 
 
